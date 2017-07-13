275eae753fb076a4d5648cf71124ef718d81ebf6
Erik Price <github@erikprice.net>
2017-07-12 19:02:55 -0700

    mix new

---
+++ lib/hermit.ex
@@ -0,0 +1,18 @@
+defmodule Hermit do
+  @moduledoc """
+  Documentation for Hermit.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hermit.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hermit_test.exs
@@ -0,0 +1,8 @@
+defmodule HermitTest do
+  use ExUnit.Case
+  doctest Hermit
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
